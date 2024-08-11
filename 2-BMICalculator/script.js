339f8c19f51fe4131c44617b5e9a3d3b35b83379
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-08-11 12:05:00 +0000

    bmi calculator

---
+++ 2-BMICalculator/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+
+// this usecase will give you empty
+//   const height = parseInt(document.querySelector('#height').value);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height <= 0 || isNaN(height)) {
+    results.innerHTML = `Please give valid height ${height}`;
+  } else if (weight === '' || weight <= 0 || isNaN(weight)) {
+    results.innerHTML = `Please give valid weight ${weight}`;
+  } else {
+    const bmi =(weight / ((height * height) / 10000)).toFixed(2);
+    // show the result
+
+    // results.innerHTML = `<span> ${bmi}</span>`;
+
+    if(bmi <18.6){
+      results.innerHTML=`<span> Under Weight: ${bmi}</span `
+    }
+    else if(bmi>24.9){
+      results.innerHTML=`<span> Over Weight: ${bmi} </span `
+    }
+   else{
+      results.innerHTML=`<span> Normal Range: ${bmi}</span `
+    }
+  }
+});
